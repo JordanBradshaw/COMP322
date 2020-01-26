2ebefd53aeb330bd59718351634bd238787f5b95
yups <yups@192.168.1.6>
2020-01-26 11:21:48 -0800

    Still working on Parity but ASCII is complete

--- LAB0/main.c
+++ LAB0/main.c
@@ -13,7 +13,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
+#include<string.h>
 
 /*
  * 
@@ -25,26 +26,43 @@ int main(int argc, char** argv) {
     scanf("%s", tempL);
     FILE *fileL = fopen(tempL, "r"); //read
 
-    if (fileL == NULL){ //if file fails run this
+    if (fileL == NULL) { //if file fails run this
         printf("Could not find file\n");
         exit(-1);
     }
-    while (fscanf(fileL, "%d", & number) ==1){
-    printf("Line that's contained in text file is:\n%d ", number);
+    while (fscanf(fileL, "%d", & number) == 1) {
+        printf("Line that's contained in text file is:\n%d ", number);
     }
-    printf("Decimal: %d", convert(number));
-    
-    
+    printf("Decimal: %d", ConvertD(number));
+    printf("ASCII is: %c", ConvertD(number));
+    printf("Parity is: %d", GetParity(number));
     return (EXIT_SUCCESS);
 }
 
-    int convert(int n){ //Convert from binary to 
-        int convertD = 0, i = 0, remainder;
-        while (n !=0){
-            remainder = n %10;
-            n /=10;
-            convertD += remainder * pow(2,i);
-            ++i;
-        } return convertD;
-        
+int ConvertD(int n) { //Convert from binary to decimal
+    int convertD = 0, i = 0, remainder;
+    while (n != 0) {
+        remainder = n % 10;
+        n /= 10;
+        convertD += remainder * pow(2, i);
+        ++i;
     }
+    return convertD;
+
+}
+void GetParity(int n){
+    int parity =0;
+    int intlength = strlen(n);
+    char array[sizeof(n)];
+    snprintf(array, "%d" , n);
+    for (int i = 0; i < intlength; i++){
+        if (array[i] == '1'){
+            parity++;
+        }else{
+            
+        }
+    }
+    
+    
+    
+}
