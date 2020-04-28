53c8c5bab06d791753b1a0a1a01646af723abd19
yupps <JordanBradshaw27@aol.com>
2020-04-28 16:16:23 -0700

    Switched Everything to Ubuntu

--- COMP322/Lab4/dining-p.c
+++ COMP322/Lab4/dining-p.c
@@ -24,11 +24,12 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include <pthread.h>
-#define LEFT (phil->current % phil->total)
-#define RIGHT (phil->current+1 % phil->total)
+
 #define THINKING 2
 #define HUNGRY 1
 #define EATING -1
+#define LEFT (phil->current % phil->total)
+#define RIGHT ((phil->current+1) % phil->total)
 
 static int termcounter = 0;
 sem_t mutex;
@@ -43,6 +44,8 @@ struct thinker {
     int total;
     int current;
     int cycles;
+    int left;
+    int right;
     sem_t** stix;
 };
 
@@ -53,15 +56,17 @@ const char* signalReturnVal(int tempSignal) {//CONVERTING SIGNALS TO STRINGS
 }
 
 void eat(struct thinker * phil) {
-    if (sem_wait(phil->stix[LEFT]) != EATING && (sem_wait(phil->stix[RIGHT]) != EATING)) {
+    if (sem_wait(phil->stix[LEFT]) != EATING
+            && sem_wait(phil->stix[RIGHT]) != EATING) {
+        perror("hi");
         printf("Philosopher %d is eating.\n", phil->current);
         usleep(rand() % 1000000);
     }
-
 }
 
 void think(struct thinker * phil) {
-    if (sem_post(phil->stix[LEFT]) != EATING && (sem_post(phil->stix[RIGHT]) != EATING)) {
+    if (sem_post(phil->stix[LEFT]) != EATING &&
+            sem_post(phil->stix[RIGHT]) != EATING) {
         printf("Philosopher %d is thinking.\n", phil->current);
         usleep(rand() % 1000000);
     }
@@ -86,28 +91,30 @@ void philCycle(struct thinker * phil) {
 }
 
 void philBuild(char** argv, struct thinker * phil) {
-    char stixBuffer[20];
+    char stixBuffer[30];
     phil->total = atoi(argv[1]);
     phil->current = atoi(argv[2]);
     phil->stix = malloc(sizeof (sem_t *) * phil->total);
     for (int i = 0; i < phil->total; ++i) {
-        memset(stixBuffer, 0, 20);
+        memset(stixBuffer, 0, 30);
         sprintf(stixBuffer, "chopStickNum%d", i);
         phil->stix[i] = sem_open(stixBuffer, O_CREAT, 0644, 1);
         //printf("Chopstick%d\n", i);
     }
+    phil->left = LEFT;
+    phil->right = RIGHT;
 }
 
 void breakStix(struct thinker * phil) {
-    char stixBuffer[20];
+    char stixBuffer[30];
     for (int i = 0; i < phil->total; ++i) {
-        if (sem_close(phil->stix[i] == -1)) {
-            perror("Chopstick close error");
+        if (sem_close(phil->stix[i]) == -1) {
+            perror("Chopstick Close");
         }
-        memset(stixBuffer, 0, 20);
-        sprintf(stixBuffer,"chopStickNum%d",i);
+        memset(stixBuffer, 0, 30);
+        sprintf(stixBuffer, "chopStickNum%d", i);
     }
-    if (sem_unlink(stixBuffer) == -1){
+    if (sem_unlink(stixBuffer) == -1) {
         //perror("ChopStick unlink error");
     }
 }
@@ -120,7 +127,6 @@ void groupFile() {
     if (stat("groupID.txt", &buf) == 0) {
         fptr = fopen("groupID.txt", "r");
         getline(&line, &len, fptr);
-        //printf("%d", getpid());
         if (setpgid(getpid(), strtol(line, NULL, 10)) != 0) {
             perror("setpgid() error");
         }
@@ -135,12 +141,18 @@ void groupFile() {
     }
 }
 
+void structCheck(struct thinker * phil) {
+    printf("Current:%d Total:%d Left:%d Right:%d\n", phil->current, phil->total, phil->left, phil->right);
+}
+
 int main(int argc, char** argv) {
+    struct thinker phil;
     printf("PID: %d\n", getpid());
     groupFile();
-    struct thinker phil;
     philBuild(argv, &phil);
+    //leftRightCalculate(&phil);
     signal(SIGTERM, handler);
+    structCheck(&phil);
     philCycle(&phil);
     breakStix(&phil);
     fprintf(stderr, "Philosopher %d has cycled %d times.\n", phil.current, phil.cycles);
