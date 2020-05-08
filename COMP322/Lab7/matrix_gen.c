09474c28d82ae1e04c132d3117742b61b25692cd
yupps <JordanBradshaw27@aol.com>
2020-05-08 14:36:21 -0700

    Gen uses async to stdout

--- COMP322/Lab7/matrix_gen.c
+++ COMP322/Lab7/matrix_gen.c
@@ -13,26 +13,41 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/aio.h>
+#include <errno.h>
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    FILE *fp = fopen("matrix.txt", "w+");
+    struct aiocb cbOut;
+    int fdOut = fileno(stdout);
     int num;
-    //int i;
     int N = atoi(argv[1]);
-    if (fp == NULL) {
-        fprintf(stderr, "Error creating file");
-    }
-    if (fp != 0) {
+    char buffer[N];
+    if (fdOut != 0) {
         for (int j = 0; j < N; j++) {
             for (int i = 0; i < N; i++) {
-                num = rand()%200 - 100;
-                fprintf(fp, "%d ", num);
+                num = rand() % 200 - 100;
+                printf("%d ", num);
             }
-            fprintf(fp, "\n");
+            printf("\n");
         }
+    }
+    bzero((char*) &cbOut, sizeof (struct aiocb));
+    cbOut.aio_buf = buffer;
+    cbOut.aio_fildes = fdOut;
+    cbOut.aio_nbytes = N;
+    int ret2 = aio_write(&cbOut);
+    if (ret2 < 0) {
+        fprintf(stderr, "AIO read failed");
+    }
+    while (aio_error(&cbOut) == EINPROGRESS) {
+    }
+    if ((ret2 = aio_return(&cbOut)) > 0) {
+        fprintf(stderr, "Success");
+    } else {
+        fprintf(stderr, "Fail");
     }
 
     return (EXIT_SUCCESS);
