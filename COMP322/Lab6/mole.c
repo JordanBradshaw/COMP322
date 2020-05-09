ba68407732ca175a5b30fc5698d692838e043396
yupps <JordanBradshaw27@aol.com>
2020-05-08 21:05:34 -0700

    apparently I cant read

--- COMP322/Lab6/mole.c
+++ COMP322/Lab6/mole.c
@@ -18,17 +18,18 @@
 
 int main(int argc, char **argv) {
     FILE *fp = fopen("lab6.log", "a");
-    if (fp == NULL){
+    if (fp == NULL) {
         fprintf(stderr, "Lab creation failed");
     }
     if (fp != 0) {
         char *mole;
         if (argc > 1 && strcmp(argv[1], "1") == 0)
-            mole = "Pop mole1";
+            mole = "Pop mole1\n";
         else
-            mole = "Pop mole2";
-        fclose(fp);
+            mole = "Pop mole2\n";
+        fputs(mole, fp);
     }
+    fclose(fp);
     return 0;
 }
 
